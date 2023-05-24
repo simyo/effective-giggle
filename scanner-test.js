3b91f4aa84cfa39550fdc6f7186eed1737217026
Simon Lange <simon.lange@ib-it.com>
2023-05-24 15:51:10 +0200

    2023-05-24 15:51:10

--- scanner-test.js
+++ scanner-test.js
@@ -6,8 +6,16 @@ const marker = document.querySelector('.overlay .bottom-right')
 const img = document.querySelector('img');
 const text = document.querySelector('#text')
 const video = document.querySelector('video')
+const allEvents = el => {
+  console.log('adding allEvents', el)
+  for (const key in el) {
+    if (/^on/.test(key))
+      el.addEventListener(key.slice(2), console.log);
+  }
+}
 const btn = document.querySelector('button')
-
+const slider = document.querySelector('input[type=range][name=scan-size]')
+const overlay = document.querySelector('.overlay')
 const log = txt => {
   console.log(txt)
   text.value += txt + '\n'
@@ -29,10 +37,32 @@ const stream = await scanner.stream({
     frameRate: { ideal: 10, },
   }
 })
+const track = stream.getTracks()[0]
+const lerp = (min, max, val) => {
+  return min * (1 - val) + max * val
+}
+console.log(video.clientWidth, video.clientHeight)
+let min = Math.max(video.clientWidth * .1, video.clientHeight * .1, 50)
+let max = Math.min(video.clientWidth * .9, video.clientHeight * .9, 500)
+const setSize = (x, y) => {
+  console.log('setSize', x, y)
+  overlay.style.setProperty('--border-space-h', `${x}px`)
+  overlay.style.setProperty('--border-space-v', `${y}px`)
+}
+video.addEventListener('resize', () => {
+  min = Math.max(video.clientWidth * .1, video.clientHeight * .1, 50)
+  max = Math.min(video.clientWidth * .9, video.clientHeight * .9, 500)
+  setSize(lerp(min, max, getSliderMult(slider)), lerp(min, max, getSliderMult(slider)))
+})
+const getSliderMult = el => (el.value - el.min) / (el.max - el.min)
+setSize(lerp(min, max, getSliderMult(slider)), lerp(min, max, getSliderMult(slider)))
+slider.addEventListener('change', e => {
+  const newMult = getSliderMult(e.target)
+  const lerpVal = lerp(min, max, newMult)
+  setSize(lerpVal, lerpVal)
+})
 
-const x = Math.min(video.clientWidth * .8, video.clientHeight * .8, 200)
-document.querySelector('.overlay').style.setProperty('--border-space-h', `${x}px`)
-document.querySelector('.overlay').style.setProperty('--border-space-v', `${x}px`)
+
 
 const hints = new Map()
 const formats = [ZXing.BarcodeFormat.DATA_MATRIX, ZXing.BarcodeFormat.CODE_128]
@@ -41,19 +71,7 @@ hints.set(ZXing.DecodeHintType.POSSIBLE_FORMATS, formats)
 hints.set(ZXing.DecodeHintType.TRY_HARDER, true)
 
 let reader = new ZXingBrowser.BrowserMultiFormatReader(hints)
-//const img2 = document.createElement("img")
-img.addEventListener('load', () => {
-  //img2.src = img.src
-})
-/*img2.addEventListener('load', () => {
-  const result = zReader.decodeFromImageUrl(img.src)
-    .then(result => {
-      debugger
-      log(result)
-    }, err => null)
-})*/
 
-const track = stream.getTracks()[0]
 const Scheduler = () => {
   let handle = null
   const toggle = () => {
@@ -65,8 +83,8 @@ const Scheduler = () => {
     handle = setInterval(() => {
       const width = (marker.offsetLeft + marker.offsetWidth) * 2
       const height = (marker.offsetTop + marker.offsetHeight) * 2
-      const randomMultX = (Math.random() * .1) + .95
-      const randomMultY = (Math.random() * .1) + .95
+      const randomMultX = (Math.random() * .2) + .9
+      const randomMultY = (Math.random() * .2) + .9
       const cutWidth = Math.min(video.clientWidth, randomMultX * width)
       const cutHeight = Math.min(video.clientHeight, randomMultY * height)
       const canvas = window.canvas = scanner.generateCanvasFromVideo({
@@ -78,7 +96,7 @@ const Scheduler = () => {
       //img.src = canvas.toDataURL()
       try {
         const result = log(JSON.stringify(reader.decodeFromCanvas(canvas)))
-      } catch(e) {
+      } catch (e) {
         ;
       }
     }, 200)
@@ -88,6 +106,6 @@ const Scheduler = () => {
 
 const scheduler = Scheduler()
 scheduler.toggle()
-btn.addEventListener('click', scheduler.toggle)
+/*btn.addEventListener('click', scheduler.toggle)
 video.addEventListener('click', scheduler.toggle)
-img.addEventListener('click', scheduler.toggle)
+img.addEventListener('click', scheduler.toggle)*/
